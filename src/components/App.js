a7f76146326ac289fd0caddaa956b9fa0d5a0474
AnuragS01 <59524154+AnuragS01@users.noreply.github.com>
2021-02-15 23:11:35 +0530

    changes saved

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+const [text,newText] = useState("");
+
+// handleClick=()=>newText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
+ 
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={()=>newText("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy")}>click </button>
+      <h1 id="para">{text}</h1>
     </div>
   );
 }
